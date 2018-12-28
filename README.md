defdb23fa9223acebca8004446da4b253d849706
jjwmezun <jjwmezun@gmail.com>
2018-12-27 17:40:35 -0800

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+WP Upload Picture
+=========================
+
+Class for autogenerating picture tag HTML for WordPress media image with just the ID.
---
+++ tests/WPUploadPictureTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use WaughJ\WPUploadPicture\WPUploadPicture;
+
+require_once( 'MockWordPress.php' );
+
+class WPUploadPictureTest extends TestCase
+{
+	public function testBasicPicture()
+	{
+		$picture = new WPUploadPicture( 2 );
+		$this->assertContains( ' srcset="https://www.example.com/wp-content/uploads/2018/12/photo-300x300.jpg?m=', $picture->getHTML() );
+	}
+}
