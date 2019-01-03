73f42705cd74830c9e93a10449859902f3c28ec2
Jaimeson Waugh <jaimeson@4cesi.com>
2019-01-03 09:49:09 -0800

    Fix attributes problem

--- src/WPUploadPicture.php
+++ src/WPUploadPicture.php
@@ -7,17 +7,21 @@ namespace WaughJ\WPUploadPicture
 	use WaughJ\HTMLImage\HTMLImage;
 	use WaughJ\HTMLPicture\HTMLPictureSource;
 	use function WaughJ\WPGetImageSizes\WPGetImageSizes;
+	use function WaughJ\TestHashItem\TestHashItemArray;
 
 	class WPUploadPicture extends HTMLPicture
 	{
 		public function __construct( int $id, array $attributes = [] )
 		{
-			$sources = self::getSources( $id );
-			$fallback_image = new HTMLImage( $sources[ 0 ]->getSrcSet() );
+			$sources = self::getSources( $id, TestHashItemArray( $attributes, 'source-attributes', [] ) );
+			$fallback_image = new HTMLImage( $sources[ 0 ]->getSrcSet(), null, TestHashItemArray( $attributes, 'img-attributes', [] ) );
+			$picture_attributes = TestHashItemArray( $attributes, 'picture-attributes', [] );
+			unset( $attributes[ 'img-attributes' ], $attributes[ 'source-attributes' ], $attributes[ 'picture-attributes' ] );
+			$attributes = array_merge( $picture_attributes, $attributes );
 			parent::__construct( $fallback_image, $sources, $attributes );
 		}
 
-		private static function getSources( int $id ) : array
+		private static function getSources( int $id, array $attributes ) : array
 		{
 			$sources = [];
 			$image_sizes = WPGetImageSizes();
@@ -32,7 +36,7 @@ namespace WaughJ\WPUploadPicture
 				$media = ( $i === $number_of_image_sizes - 1 && $i > 0 )
 					? "(min-width:{$min_width}px)"
 					: "(max-width:{$max_width}px)";
-				$sources[] = new HTMLPictureSource( $url, $media );
+				$sources[] = new HTMLPictureSource( $url, $media, $attributes );
 				$min_width = $max_width + 1;
 			}
 			return $sources;
--- tests/WPUploadPictureTest.php
+++ tests/WPUploadPictureTest.php
@@ -9,9 +9,15 @@ class WPUploadPictureTest extends TestCase
 {
 	public function testBasicPicture()
 	{
-		$picture = new WPUploadPicture( 2 );
+		$picture = new WPUploadPicture( 2, [ 'img-attributes' => [ 'class' => 'something' ], 'source-attributes' => [ 'class' => 'something-else' ], 'picture-attributes' => [ 'class' => 'major' ] ] );
 		$this->assertContains( ' srcset="https://www.example.com/wp-content/uploads/2018/12/photo-300x300.jpg"', $picture->getHTML() );
 		$this->assertContains( ' media="(max-width:300px)"', $picture->getHTML() );
 		$this->assertContains( ' media="(min-width:769px)"', $picture->getHTML() );
+		$this->assertContains( ' class="something"', $picture->getFallbackImage()->getHTML() );
+		$this->assertContains( ' class="something-else"', $picture->getHTML() );
+		$this->assertContains( ' class="major"', $picture->getHTML() );
+		$this->assertNotContains( 'img-attributes="', $picture->getHTML() );
+		$this->assertNotContains( 'source-attributes="', $picture->getHTML() );
+		$this->assertNotContains( 'picture-attributes="', $picture->getHTML() );
 	}
 }
